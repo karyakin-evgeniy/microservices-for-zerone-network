ee89c8c638388c4cfe9528967bde8a697baaea61
karyakin-evgeniy <84029079+karyakin-evgeniy@users.noreply.github.com>
2022-08-08 09:51:26 +0300

    Delete utils directory

--- utils/declOfNum.js
+++
@@ -1,7 +0,0 @@
-export default function declOfNum(number, words) {
-    const condition = (number % 100 > 4 && number % 100 < 20);
-    const value1 = 2;
-    const value2 = [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5];
-    const index = condition ? value1 : value2;
-    return words[index];
-}
