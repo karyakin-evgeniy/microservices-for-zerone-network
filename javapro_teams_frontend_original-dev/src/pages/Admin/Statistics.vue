34fcd93d7052060520af9a97cd0b0de7ed092178
karyakin-evgeniy <84029079+karyakin-evgeniy@users.noreply.github.com>
2022-08-17 10:22:23 +0300

    Delete javapro_teams_frontend_original-dev directory

--- javapro_teams_frontend_original-dev/src/pages/Admin/Statistics.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-  h1 Statistics
-</template>
-
-<script>
-export default {
-  name: 'AdminStatistics'
-}
-</script>
--- javapro_teams_frontend_original-dev/src/utils/notifications.utils.js
+++
@@ -1,8 +0,0 @@
-export function getRouteByNotification(notification) {
-    switch (notification.event_type) {
-        case 'MESSAGE':
-            return { name: 'Im', params: { id: notification.entity_id } }
-        default:
-            return { name: 'ProfileId', params: { id: notification.entity_id } }
-    }
-}
