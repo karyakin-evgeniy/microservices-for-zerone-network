f65339aeb8610fb3acc0f661e07712dd826f1048
karyakin.ea <karykin.ea@gmail.com>
2022-09-25 13:29:10 +0300

    add bootstrap.yml

--- reports/src/main/java/org/proteam24/reports/controller/ReportController.java
+++ reports/src/main/java/org/proteam24/reports/controller/ReportController.java
@@ -1,6 +1,8 @@
 package org.proteam24.reports.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.proteam24.reports.entity.ReportEntity;
 import org.proteam24.reports.repository.ReportRepository;
@@ -15,17 +17,20 @@ import java.security.Principal;
 @RestController
 @RequestMapping(value = "/reports")
 @RequiredArgsConstructor
+@Tag(name = "Жалобы", description = "Работа с жалобами")
 public class ReportController {
 
     private final ReportService reportService;
 
 
     @PutMapping("/")
+    @Operation(summary = "Получение отчёта")
     public ResponseEntity putReport(@RequestBody ReportEntity reportEntity, Principal principal) {
         return reportService.putReport(reportEntity,principal);
     }
 
     @GetMapping("/")
+    @Operation(summary = "Получить все отчёты")
     public ResponseEntity getAllReports() {
         return reportService.getAllReports();
     }
