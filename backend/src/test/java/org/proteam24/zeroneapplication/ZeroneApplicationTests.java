1720511525a9df4f267f7a12e5da805f2f544895
karyakin.ea <karykin.ea@gmail.com>
2022-09-25 13:30:32 +0300

    add tests

---
+++ backend/src/test/java/org/proteam24/zeroneapplication/ZeroneApplicationTests.java
@@ -0,0 +1,14 @@
+package org.proteam24.zeroneapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroneApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
